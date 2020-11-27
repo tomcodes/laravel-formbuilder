eeb54b1ba4bc506f16a45ee31d6a6205c91d647c
tomcodes <tomcodes@users.noreply.github.com>
2020-11-27 15:43:23 +0100

    Fix FormBuilderServiceProvider

--- src/FormBuilderServiceProvider.php
+++ src/FormBuilderServiceProvider.php
@@ -12,54 +12,53 @@ use jazmy\FormBuilder\Middlewares\PublicFormAccess;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
-
 class FormBuilderServiceProvider extends ServiceProvider
 {
-	/**
-	 * Register bindings in the container.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-	    $this->mergeConfigFrom(
-	        __DIR__.'/../config/config.php', 'formbuilder'
-	    );
-	}
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/config.php', 'formbuilder'
+        );
+    }
 
-	/**
-	 * Perform post-registration booting of services.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-	    // load custom route overrides
-	    $this->loadRoutesFrom( __DIR__.'/../routes.php' );
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // load custom route overrides
+        $this->loadRoutesFrom( __DIR__.'/../routes.php' );
 
-	    // register the middleware
-	    Route::aliasMiddleware('public-form-access', PublicFormAccess::class);
-	    Route::aliasMiddleware('submisson-editable', FormAllowSubmissionEdit::class);
+        // register the middleware
+        Route::aliasMiddleware('public-form-access', PublicFormAccess::class);
+        Route::aliasMiddleware('submisson-editable', FormAllowSubmissionEdit::class);
 
-	    // load migrations
-	    $this->loadMigrationsFrom( __DIR__.'/../migrations' );
+        $this->publishes([
+            __DIR__.'/../database/migrations/2018_09_30_110932_create_forms_table.php' => database_path('migrations/2018_09_30_110932_create_forms_table.php'),
+            __DIR__.'/../database/migrations/2018_09_30_142113_create_form_submissions_table.php' => database_path('migrations/2018_09_30_142113_create_form_submissions_table.php'),
+            __DIR__.'/../database/migrations/2018_10_16_000926_add_custom_submit_url_column_to_the_forms_table.php' => database_path('migrations/2018_10_16_000926_add_custom_submit_url_column_to_the_forms_table.php'),
+        ], 'migrations');
 
-	    // load the views
-	    $this->loadViewsFrom( __DIR__.'/../views', 'formbuilder' );
+        // // publish config files
+        $this->publishes([
+            __DIR__.'/../config/config.php' => config_path('formbuilder.php', 'formbuilder'),
+        ], 'formbuilder-config');
 
-	    // publish config files
-	    $this->publishes([
-	        __DIR__.'/../config/config.php' => config_path('formbuilder.php', 'formbuilder'),
-	    ], 'formbuilder-config');
+        // publish view files
+        $this->publishes([
+            __DIR__.'/../views' => resource_path('views/vendor/formbuilder', 'formbuilder::'),
+        ], 'formbuilder-views');
 
-	    // publish view files
-	    $this->publishes([
-	    	__DIR__.'/../views' => resource_path('views/vendor/formbuilder', 'formbuilder::'),
-	    ], 'formbuilder-views');
-
-	    // publish public assets
-	    $this->publishes([
-	        __DIR__.'/../public' => public_path('vendor/formbuilder'),
+        // // publish public assets
+        $this->publishes([
+            __DIR__.'/../public' => public_path('vendor/formbuilder'),
         ], 'formbuilder-public');
-	}
+    }
 }
